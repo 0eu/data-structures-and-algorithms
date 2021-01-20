2548973cbc9af3825228c3c03eaaaa4c71a381d5
0eu <eugene@fup.app>
2021-01-20 20:24:33 +0300

    Implement Push method

---
+++ data-structures/Stack/stack.go
@@ -0,0 +1,69 @@
+package stack
+
+import (
+	list "container/list"
+	"errors"
+)
+
+var (
+	ErrorExceededCapacity = errors.New("capacity is exceeded")
+)
+
+// Stack is an ADT.
+type Stack interface {
+	Push(element interface{}) error
+	Peek() (interface{}, error)
+	Pop() (interface{}, error)
+	IsFull() bool
+	IsEmpty() bool
+	Size() int
+}
+
+type LinkedListStack struct {
+	container *list.List
+	length    int
+	capacity  int
+}
+
+func NewLinkedListStack(capacity int) *LinkedListStack {
+	return &LinkedListStack{
+		container: list.New(),
+		length:    0,
+		capacity:  capacity,
+	}
+}
+
+func (s *LinkedListStack) Push(element interface{}) error {
+	if s.IsFull() {
+		return ErrorExceededCapacity
+	}
+	s.container.PushBack(element)
+	s.length++
+	return nil
+}
+
+func (s *LinkedListStack) IsFull() bool {
+	if s.length < s.capacity {
+		return false
+	}
+	return true
+}
+
+func (s *LinkedListStack) IsEmpty() bool {
+	if s.length == 0 {
+		return true
+	}
+	return false
+}
+
+func (s *LinkedListStack) Size() int {
+	return s.length
+}
+
+func (s *LinkedListStack) Pop() (interface{}, error) {
+	return 0, nil
+}
+
+func (s *LinkedListStack) Peek() (interface{}, error) {
+	return 0, nil
+}
