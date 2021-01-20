184bc027396255fb852cf06be8addca3a855acb7
0eu <eugene@fup.app>
2021-01-20 20:25:29 +0300

    Add test case to check if max capacity is exceeded

--- data-structures/Stack/stack_test.go
+++ data-structures/Stack/stack_test.go
@@ -10,12 +10,32 @@ func assertLength(t *testing.T, stack Stack, expected int) {
 	}
 }
 
+func assertError(t *testing.T, actual, expected error) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("expected error %s, but got: %s", expected, actual)
+	}
+}
+
 func TestPush(t *testing.T) {
-	t.Run("should push an element onto a stack", func(t *testing.T) {
-		stack := NewLinkedListStack()
+	t.Run("Push should put an element onto a stack", func(t *testing.T) {
+		stack := NewLinkedListStack(1)
 
-		stack.Push(1)
+		_ = stack.Push(1)
 
+		assertLength(t, stack, 1)
+	})
+
+	t.Run("Push should exceeded a stack's capacity", func(t *testing.T) {
+		stack := NewLinkedListStack(1)
+
+		err := stack.Push(1)
+
+		assertError(t, err, nil)
+
+		err = stack.Push(1)
+
+		assertError(t, err, ErrorExceededCapacity)
 		assertLength(t, stack, 1)
 	})
 }
