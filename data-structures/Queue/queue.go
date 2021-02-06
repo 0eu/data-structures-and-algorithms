d746b558d22ff0253cb8df52ea3cdb8f5ee54170
0eu <eugene@fup.app>
2021-02-06 14:09:33 +0300

    Add errors for queue

--- data-structures/Queue/queue.go
+++ data-structures/Queue/queue.go
@@ -1,9 +1,17 @@
 package queue
 
+import "errors"
+
+var (
+	ErrorExceededCapacity = errors.New("capacity is exceeded")
+	ErrorWrongCapacity    = errors.New("capacity should be in range [1, 1024]")
+	ErrorEmptyQueue       = errors.New("the operation can't be permitted on an empty queue")
+)
+
 type Queue interface {
 	Enqueue(element interface{}) error
 	Dequeue() (interface{}, error)
-	IsFull() bool
+	Peek() (interface{}, error)
 	IsEmpty() bool
 	Size() int
 }
