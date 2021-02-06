a6fe205a27250d37e15bb7ca82feaaee1b3ce4f5
0eu <eugene@fup.app>
2021-02-06 15:34:29 +0300

    Implement a skeleton of LinkedList

---
+++ data-structures/LinkedList/linked_list.go
@@ -0,0 +1,39 @@
+package LinkedList
+
+// Node is a unit in a linked list, that stores data and a pointer to the next element.
+type Node struct {
+	Value interface{}
+	Next  *Node
+}
+
+// LinkedList is a struct that has pointers to the first and last nodes and also size of a list.
+type LinkedList struct {
+	Head *Node
+	Tail *Node
+	size int
+}
+
+// NewLinkedList builds an empty linked list and returns its pointer.
+func NewLinkedList() *LinkedList {
+	return &LinkedList{
+		Head: nil,
+		Tail: nil,
+		size: 0,
+	}
+}
+
+// Size returns the number of nodes in a list.
+func (l *LinkedList) Size() int {
+	return l.size
+}
+
+// Append adds an element to the end of a list.
+func (l *LinkedList) Append(value interface{}) {
+	current, newNode := l.Head, &Node{Value: value, Next: nil}
+	if current != nil {
+		l.Tail.Next, l.Tail = newNode, newNode
+	} else {
+		l.Head, l.Tail = newNode, newNode
+	}
+	l.size++
+}
